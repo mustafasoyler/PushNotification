171c3422d7068fba46b7ea34d466fcb31697b9cd
mustafasoyler <ali@orhun.net>
2020-08-04 16:49:32 +0300

    Initial commit

---
+++ app/src/main/java/com/example/pushnotification/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.pushnotification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.onesignal.OneSignal;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
+    }
+}
